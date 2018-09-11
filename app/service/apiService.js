0a55ce30298f150ed69ce5197f25233b6a794cdb
Gary Zhu <dinner3000@hotmail.com>
2018-09-11 20:22:07 +0800

    feat: add baidu api call for collecting city x/y coordinates

--- app/service/apiService.js
+++ app/service/apiService.js
@@ -3,7 +3,10 @@
 const model = require('../model');
 const IpLocationService = require('./ipLocationService');
 const config = require('../config');
+
 const MariadbClient = require('mariasql');
+const request = require("request");
+
 const logger = require('./logService')('apiService.js');
 
 class ApiService {
@@ -15,12 +18,46 @@ class ApiService {
 
     addSurveySubmit(params) {
 
-        logger.debug(params);
+        let _this = this;
 
         let locationService = new IpLocationService();
         let location = locationService.findCityByIp(params.ip);
-        logger.debug(location);
 
+        let client = new MariadbClient(config.MARIADB);
+
+        let insertSubmittedLocationTemplate = client.prepare(
+            'INSERT INTO `submitted_location` ' +
+            '(`name`, `ip`, `datetime`, `city`, `location`) ' +
+            'VALUES (:name, :ip, :datetime, :city, :location)');
+
+        let insertCityPointTemplate = client.prepare(
+            'INSERT INTO `city_point` ' +
+            '(`city`, `x`, `y`, `location`) ' +
+            'VALUES (:city, :x, :y, :location)');
+
+        request('http://api.map.baidu.com/location/ip?ip=1.180.120.242&ak=4SUOswuyXWNUV5kSUGoxgQx4ms7lamxh&coor=bd09ll', 
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    // logger.debug(body) // Show the HTML for the baidu homepage.
+                    let jsonResult = JSON.parse(body);
+                    // logger.debug(jsonResult);
+                    if(jsonResult && jsonResult.status == 0){
+                        // logger.debug(jsonResult.content.address_detail.city);
+                        // logger.debug(jsonResult.content.point);
+                        client.query(insertCityPointTemplate({city: location.city, x: jsonResult.content.point.x, y: jsonResult.content.point.y, location: JSON.stringify(jsonResult)}), 
+                            function (e, rows) {
+                                if (e) {
+                                    logger.error(e);
+                                }else{
+                                    logger.debug('insertCityPointTemplate');
+                                    logger.debug(rows);
+                                }
+                        });
+                    }
+                }
+            }
+        );
+        
         let surveySubmit = new model.SurveySubmit({
             name: params.name,
             ip: params.ip,
@@ -28,24 +65,13 @@ class ApiService {
             city: location.city,
             location: JSON.stringify(location.detail)
         });
-
-        logger.debug(surveySubmit);
-
-        let client = new MariadbClient(config.MARIADB);
-
-        let insertTemplate = client.prepare(
-            'INSERT INTO `submitted_location` ' +
-            '(`name`, `ip`, `datetime`, `city`, `location`) ' +
-            'VALUES (:name, :ip, :datetime, :city, :location)');
-
-        let _this = this;
-        // logger.debug(_this)
-        client.query(insertTemplate(surveySubmit), function (e, rows) {
+        client.query(insertSubmittedLocationTemplate(surveySubmit), function (e, rows) {
             if (e) {
                 logger.error(e);
                 _this.responsor.sendResponse(400, e.message);
             }else{
-                // logger.debug(rows);
+                logger.debug('insertSubmittedLocationTemplate');
+                logger.debug(rows);
                 _this.responsor.sendResponse(200, rows);
             }
         });
@@ -67,7 +93,11 @@ class ApiService {
         let client = new MariadbClient(config.MARIADB);
 
         // logger.debug(name);
-        let selectTemplate = client.prepare('SELECT `city`, count(*) as count FROM `submitted_location` WHERE `city` IS NOT NULL AND `city` != "" AND `name` = :name GROUP BY `city`');
+        let selectTemplate = client.prepare(
+            'SELECT s.`city`, COUNT(*) AS count, p.`x`, p.`y` FROM `submitted_location` AS s LEFT JOIN `city_point` AS p ON s.`city` = p.`city` ' + 
+            'WHERE s.`name` = :name AND s.`city` IS NOT NULL AND s.`city` != "" AND p.`x` IS NOT NULL AND p.`y` IS NOT NULL ' + 
+            'GROUP BY `city`');
+
         client.query(selectTemplate({name: name}), {useArray: true}, function (e, rows) {
             if (e) {
                 logger.error(e);
