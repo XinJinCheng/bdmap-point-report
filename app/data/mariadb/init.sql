8b8e53e61fbac0494f7e22d4c626c86f3559db9f
Gary Zhu <dinner3000@hotmail.com>
2018-09-13 11:35:19 +0800

    feat: update init sql

--- app/data/mariadb/init.sql
+++ app/data/mariadb/init.sql
@@ -13,12 +13,12 @@
 
 
 -- Dumping database structure for survey_report
-DROP DATABASE IF EXISTS `survey_report`;
+-- DROP DATABASE IF EXISTS `survey_report`;
 CREATE DATABASE IF NOT EXISTS `survey_report` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `survey_report`;
 
 -- Dumping structure for table survey_report.city_point
-DROP TABLE IF EXISTS `city_point`;
+-- DROP TABLE IF EXISTS `city_point`;
 CREATE TABLE IF NOT EXISTS `city_point` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `city` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
@@ -29,15 +29,8 @@ CREATE TABLE IF NOT EXISTS `city_point` (
   UNIQUE KEY `city` (`city`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table survey_report.city_point: ~1 rows (approximately)
-/*!40000 ALTER TABLE `city_point` DISABLE KEYS */;
-INSERT INTO `city_point` (`id`, `city`, `x`, `y`, `location`) VALUES
-	(1, '赤峰市', '118.93076119', '42.29711232', NULL),
-	(3, '赤峰', '118.93076119', '42.29711232', '{"address":"CN|内蒙古|赤峰|None|CHINANET|0|0","content":{"address":"内蒙古自治区赤峰市","address_detail":{"city":"赤峰市","city_code":297,"district":"","province":"内蒙古自治区","street":"","street_number":""},"point":{"x":"118.93076119","y":"42.29711232"}},"status":0}');
-/*!40000 ALTER TABLE `city_point` ENABLE KEYS */;
-
 -- Dumping structure for table survey_report.submitted_location
-DROP TABLE IF EXISTS `submitted_location`;
+-- DROP TABLE IF EXISTS `submitted_location`;
 CREATE TABLE IF NOT EXISTS `submitted_location` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -49,10 +42,6 @@ CREATE TABLE IF NOT EXISTS `submitted_location` (
   KEY `datetime` (`datetime`,`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=89 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table survey_report.submitted_location: ~5 rows (approximately)
-/*!40000 ALTER TABLE `submitted_location` DISABLE KEYS */;
-/*!40000 ALTER TABLE `submitted_location` ENABLE KEYS */;
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
@@ -61,3 +50,5 @@ CREATE TABLE IF NOT EXISTS `submitted_location` (
 CREATE USER IF NOT EXISTS 'report'@'localhost' IDENTIFIED BY '123456';
 GRANT ALL ON `survey_report`.* TO 'report'@'localhost';
 
+CREATE USER IF NOT EXISTS 'report'@'%' IDENTIFIED BY '123456';
+GRANT ALL ON `survey_report`.* TO 'report'@'%';
